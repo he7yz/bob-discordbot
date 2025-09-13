a3b6faadb0ab7c7b9f7beb3c0b7f87fb59c15e36
λz <148996310+he7yz@users.noreply.github.com>
2025-09-14 01:58:48 +0800

    fix 6

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js")
-const client = new Discord.Client({intents: [
+const client = new Discord.Client({intents: [ // new discord gateway intents
   Discord.GatewayIntentBits.Guilds,
   Discord.GatewayIntentBits.GuildMessages,
   Discord.GatewayIntentBits.MessageContent
 ]})
 
-client.once("ready", () => {
+client.once("ready", () => { //ready is deprecated, uses clientReady now
   console.log(`${client.user.tag} is up and ready!`)
 });
 
-client.on("messageCreate", (message) =>{
+client.on("messageCreate", (message) =>{ //message is also deprecated, uses messageCreate now..
   if (message.content === 'sudo ping') {
     message.channel.send("Pinging ...")
       // console.log(`Sending Ping...`)
